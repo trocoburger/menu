07859e8f5b54ebd1a17f0742f908080424b99692
trocoburger <ordenestrocoburger@gmail.com>
2020-10-08 21:30:34 -0500

    Adding padding to body. Changing transition timing.

--- scripts/menu.js
+++ scripts/menu.js
@@ -8,12 +8,14 @@ function ready(fn) {
 
   ready(function(){
       const splash = document.querySelector('.splash');
-      splash.style.setProperty('--animate-duration', '1.5s');
+      splash.style.setProperty('--animate-duration', '2.0s');
       splash.classList.add('animate__animated', 'animate__zoomIn');
       splash.addEventListener('animationend', () => {
         splash.classList.add('animate__zoomOut');
-        const menu = document.querySelector('.menu');
-        menu.style.setProperty('--animate-duration', '1.5s');
-        menu.classList.add('active', 'animate__animated', 'animate__zoomIn');
+        setTimeout(function(){
+          const menu = document.querySelector('.menu');
+          menu.style.setProperty('--animate-duration', '2.0s');
+          menu.classList.add('active', 'animate__animated', 'animate__zoomIn');
+        },800);
       });
   });
